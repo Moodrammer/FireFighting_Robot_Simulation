473198f537c5c464f3dc0232205917fd37eb875f
Moodrammer <mahmou.m.gody@gmail.com>
2020-04-17 18:21:58 +0200

    Fix: remove delaymicroseconds

--- stepper.ino
+++ stepper.ino
@@ -1,4 +1,7 @@
-
+// Notes:
+//In my solution, I assume starting with the switch simulating the fire sensor being off(The fire sensor starts with a Low signal till it senses fire)
+//During the rotation of the robot in 90 degrees in its place it doesnot check the ultrasonic sensors till rotating completely
+//When reaching a dead end the robot moves two steps backward , checks its surroundings for any escape , if it found a free track it will turn , else it moves backward again 
 #define StepsPerRev 64
 #define RotSpeed 30
 
@@ -26,7 +29,7 @@
 #define leftecho 3
 #define rightecho 0
 
-#define numStepsToRotate 128  
+#define numStepsToRotate 128  //Number of steps for two revolutions
 //-----------------------------------------------------------------------------------------------------------------------
 #include <Stepper.h>
 //------------------------------------------------------------------------------------------------------------------------
@@ -46,10 +49,11 @@ bool alerttimeset = 0;
 bool stopMotors = 0;
 int motionStatus = 0; //0: moveforward 1: turn right 2: turn left 3: movebackwards
 int rotationSteps = numStepsToRotate; //two revolutions
+//byte currentStep = 0; //used in the moveMotor function
 bool rotating = 0; //0: the robot is not rotating 1: the robot is currently applying a rotation
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(PowerSwitch, INPUT);
   
@@ -64,11 +68,12 @@ void setup() {
 void loop() {
   //if the power is on operate the robot
   if(digitalRead(PowerSwitch)){
-    //Calculate the time at the begining of the loop
+    //Calculate the number of milliseconds at the begining of the loop
     currenttime = millis();
     //---------------------------------------------------------------------------------------------------
     //Check for the distance using the front ultrasonic
-    //calculate the front distance if the robot is neither performing a rotation nor vanquishing fire
+    //Calculate the front distance if the robot is neither performing a rotation nor vanquishing fire
+    //In these cases the robot is not moving, so the ultrasonic reading is useless
     if(!rotating && !alertConfirm){
       if(calcDist(frontecho) < 10){
         if(calcDist(rightecho) < 10){
@@ -84,7 +89,7 @@ void loop() {
         }
         else{
           //turn right
-          motionStatus = 1; 
+          motionStatus = 1;
         }
       }
     }
@@ -93,19 +98,22 @@ void loop() {
     if(!stopMotors){
       leftWheel.setSpeed(RotSpeed);
       rightWheel.setSpeed(RotSpeed);
-      
+      //The robot moves according to the motionStatus specified without stopping the loop
       switch(motionStatus){
-      //move forwards
+      //move forwards-------------------------------------------
       case 0:
         //Ordinary rotation of the motor
-        rightWheel.step(StepsPerRev/ 32);
-        leftWheel.step(StepsPerRev/ 32);
+        rightWheel.step(1);
+        //moveMotor(rightWheelAp, rightWheelAm,rightWheelBp, rightWheelBm);
+        leftWheel.step(1);
+        //moveMotor(leftWheelAp, leftWheelAm, leftWheelBp, leftWheelBm);
         break;
-      //turn right  
+      //turn right 90 degrees----------------------------------------------  
       case 1:
         if(rotationSteps){
-          leftWheel.step(StepsPerRev/ 32);
-          rotationSteps -= StepsPerRev/ 32;
+          leftWheel.step(1);
+          //moveMotor(leftWheelAp, leftWheelAm, leftWheelBp, leftWheelBm);
+          rotationSteps -= 1;
           rotating = 1;
          }
         else{
@@ -115,11 +123,12 @@ void loop() {
           rotating = 0; 
         }
         break;
-      //turn left  
+      //turn left 90 degrees------------------------------------------------  
       case 2:        
         if(rotationSteps){
-          rightWheel.step(StepsPerRev/ 32);
-          rotationSteps -= StepsPerRev/ 32;
+          rightWheel.step(1);
+          //moveMotor(rightWheelAp, rightWheelAm,rightWheelBp, rightWheelBm);
+          rotationSteps -= 1;
           rotating = 1;
          }
         else{
@@ -129,11 +138,14 @@ void loop() {
           rotating = 0; 
         }
         break;
+       //move backwards two wheel revolutions-------------------------------------------
       case 3:
         if(rotationSteps){
-          rightWheel.step(-(StepsPerRev/ 32));
-          leftWheel.step(-(StepsPerRev/ 32));
-          rotationSteps -= StepsPerRev / 32;
+          rightWheel.step(-(1));
+          leftWheel.step(-(1));
+          //moveMotor(leftWheelAp, leftWheelAm, leftWheelBp, leftWheelBm);
+          //moveMotor(rightWheelAp, rightWheelAm,rightWheelBp, rightWheelBm);
+          rotationSteps -= 1;
           rotating = 1;  
         }
         else {
@@ -152,12 +164,14 @@ void loop() {
       prevtime = currenttime;
     }
     
-    //If a fire alert occurs
+    //If a fire alert occurs set the alertStarting time with the current time
     if(fireAlert && !alerttimeset) {
         alerttime = currenttime;
         alerttimeset = 1;
     }
+    //If an alertConfirm signal is asserted this means the sensor returned a low after a high signal so the duration of high signal should be checked
     if(alertConfirm){
+      //If the duration is more than half a second
       if(currenttime - alerttime >= 500){
        //stop the motors from moving
         stopMotors = 1; 
@@ -173,13 +187,15 @@ void loop() {
     }
   }
   else{
-    //force switching off the LED if the switch becomes off
+    //force switching off the LED if power the switch becomes off
     digitalWrite(ledPin, LOW);
   }
 
 }
+//--------------------------------------------------------------------------------------------------------------------------
 //ISR to trigger the status of flags when the sensor input is triggered
 void triggerStatus(){
+  //If the exit signal is high then reset its state to low when the sensor changes from high to low
   if(!exitSignal){
     //If there is no fireAlert set the fireAlert
     if(!fireAlert) fireAlert = 1;
@@ -198,7 +214,8 @@ void putoff(){
   //continue as long as no exit signal has been triggered
   if(!exitSignal){
     fan.setSpeed(RotSpeed);
-    fan.step(StepsPerRev/ 32);
+    fan.step(1);
+    //moveMotor(fanAp, fanAm,fanBp, fanBm);
   }
   else{
      //reset all the status variables
@@ -211,18 +228,22 @@ void putoff(){
 
 //Calculates the distance of the triggered ultrasonic
 float calcDist(int echopin){
-  digitalWrite( triggerPin, LOW ); 
-  delayMicroseconds( 2 );
- 
-  digitalWrite( triggerPin, HIGH );
-  delayMicroseconds( 10 );
-  digitalWrite( triggerPin, LOW );
-  
+  //Invoke the ultrasonic sensor to generate ultrasonic waves
   unsigned long triggertime;
+  triggertime = micros();
+  while(micros() - triggertime < 20){
+    digitalWrite(triggerPin, LOW);
+  }
+  triggertime = micros();
+  while(micros() - triggertime < 40){
+    digitalWrite(triggerPin, HIGH); 
+  }
+  digitalWrite(triggerPin, LOW);
+  
   float distance = 0;
   float duration = 0;
   int counter = 0;
-  
+  // the duration till the waves bounce and return to the reciever are twice the time through which the trigger pin goes to high then returns to low on recieving the wave
   switch(echopin) {
 //-----------------------------------front sensor-------------------------------------------------      
       case frontecho:
@@ -276,6 +297,42 @@ float calcDist(int echopin){
 //------------------------------------calculate the distance --------------------------------------      
   //0.0344 is the speed of ultrasonic waves in air in cm/ microsecond
   distance = (duration / 2) * 0.0344;
-  Serial.println(distance);
   return distance;
 }
+
+//Move the stepper motor
+//void moveMotor(int Ap,int Am,int Bp,int Bm){
+//  pinMode(Ap, OUTPUT);
+//  pinMode(Am, OUTPUT);
+//  pinMode(Bp, OUTPUT);
+//  pinMode(Bm, OUTPUT);
+//  switch(currentStep){
+//    case 0:
+//    digitalWrite(Ap, 1);
+//    digitalWrite(Bp, 1);
+//    digitalWrite(Am, 0);
+//    digitalWrite(Bm, 0);
+//    break;
+//  
+//    case 1:
+//    digitalWrite(Ap, 1);
+//    digitalWrite(Bp, 0);
+//    digitalWrite(Am, 0);
+//    digitalWrite(Bm, 1);
+//    break;
+//  
+//    case 2:
+//    digitalWrite(Ap, 0);
+//    digitalWrite(Bp, 0);
+//    digitalWrite(Am, 1);
+//    digitalWrite(Bm, 1);
+//    break;
+//  
+//    case 3:
+//    digitalWrite(Ap, 0);
+//    digitalWrite(Bp, 1);
+//    digitalWrite(Am, 1);
+//    digitalWrite(Bm, 0);
+//    break;
+//   }
+//}
